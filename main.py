8fb50c30eabb887a9e37760769feff3b0c7415d6
ted <eyimofeokikiola@gmail.com>
2024-03-14 23:57:12 +0100

    PUT DELETE operations

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status, HTTPException
 from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
 from random import randrange
+
 
 app =  FastAPI()
 
@@ -20,6 +21,11 @@ def find_posts(id):
     for p in my_posts:
         if p["id"] == id:
             return p
+
+def find_index_post(id):
+    for i, p in enumerate(my_posts):
+        if p['id'] == id:
+            return i
         
 @app.get("/")
 async def root():
@@ -42,7 +48,27 @@ def create_post(post: PostModel):
 
 
 @app.get("/posts/{id}")
-def get_post(id: int):
+def get_post(id: int, respose:Response):
     print(id)
     post = find_posts(id)
+    if not post:
+        raise HTTPException(status_code= status.HTTP_404_NOT_FOUND, detail="Post not found")
+    return {"message": "success", "data": post}
+
+@app.delete("/post/{id}", status_code= status.HTTP_204_NO_CONTENT)
+def delete_posts(id: int):
+    index = find_index_post(id)
+    if index ==  None: 
+        raise HTTPException(status_code= status.HTTP_404_NOT_FOUND, detail="invalid post")
+    my_posts.pop(index)
+    return Response(status_code=status.HTTP_204_NO_CONTENT) 
+
+@app.put("/posts/{id}")
+def update_posts(id: int, post:PostModel):
+    index = find_index_post(id)
+    if index ==  None: 
+        raise HTTPException(status_code= status.HTTP_404_NOT_FOUND, detail="invalid post")
+    post_dict = post.dict()
+    post_dict['id'] = id
+    my_posts[index] = post_dict
     return {"message": "success", "data": post}
