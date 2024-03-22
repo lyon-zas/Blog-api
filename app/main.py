d1a9ed78de2afbae9251ca8680d8dc164f44cdd4
ted <eyimofeokikiola@gmail.com>
2024-03-22 17:26:20 +0100

    sql connection and query function

--- app/main.py
+++ app/main.py
@@ -3,6 +3,9 @@ from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
 from random import randrange
+import psycopg
+# from psycopg2.extras import RealDictCursor
+import time
 
 
 app =  FastAPI()
@@ -12,6 +15,17 @@ class PostModel(BaseModel):
     content:str
     published: bool = True
     
+while True:
+    try:
+        conn =  psycopg.connect(host= 'localhost', dbname= 'fastapi', user='postgres', 
+                                password= 'Okikiola',)
+        cursor = conn.cursor()
+        print("Database connection was succesfull!!")
+        break
+    except Exception as error:
+        print("Connecting to database failed ")
+        print("Error", error)
+        time.sleep(2)
 
 
 
@@ -25,50 +39,54 @@ def find_posts(id):
 def find_index_post(id):
     for i, p in enumerate(my_posts):
         if p['id'] == id:
-            return i
+            return i 
         
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-@app.get("/user")
-def user():
-    return {"data":  {"user_nmae": "ted", "name": "Eyimofe"}}
 
 @app.get("/posts")
 def get_posts():
-    return {"message": "successfully retrived", "data":my_posts}
+    cursor.execute("""SELECT * FROM posts""")
+    posts = cursor.fetchall()
+    print("is it working") 
+    print(posts)
+    return {"message": "successfully retrived", "data":posts}
 
-@app.post("/posts")
+@app.post("/posts", status_code= status.HTTP_201_CREATED)
 def create_post(post: PostModel):
-    post_dict = post.dict()
-    post_dict["id"] = randrange(0,1000000)
-    my_posts.append(post_dict)
-    return {"message": "successfully created", "data": post_dict}
+    cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s,%s,%s) RETURNING *""",
+                   (post.title, post.content, post.published))
+    new_posts = cursor.fetchone()
+    conn.commit()
+    
+    return {"message": "successfully created", "data": new_posts}
 
 
 @app.get("/posts/{id}")
 def get_post(id: int, respose:Response):
     print(id)
-    post = find_posts(id)
+    cursor.execute("""SELECT * FROM posts WHERE id = %s """,(str(id),))
+    post = cursor.fetchone()
     if not post:
-        raise HTTPException(status_code= status.HTTP_404_NOT_FOUND, detail="Post not found")
+        raise HTTPException(status_code= status.HTTP_404_NOT_FOUND, detail=f"posts with the id: {id} does not exist")
     return {"message": "success", "data": post}
 
 @app.delete("/post/{id}", status_code= status.HTTP_204_NO_CONTENT)
 def delete_posts(id: int):
-    index = find_index_post(id)
-    if index ==  None: 
-        raise HTTPException(status_code= status.HTTP_404_NOT_FOUND, detail="invalid post")
-    my_posts.pop(index)
+    print(id)
+    cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING *""", (str(id),))
+    deleted_post = cursor.fetchone()
+    conn.commit()
+    if deleted_post ==  None: 
+        raise HTTPException(status_code= status.HTTP_404_NOT_FOUND, detail=f"posts with the id: {id} does not exist")
+   
     return Response(status_code=status.HTTP_204_NO_CONTENT) 
 
 @app.put("/posts/{id}")
 def update_posts(id: int, post:PostModel):
-    index = find_index_post(id)
-    if index ==  None: 
-        raise HTTPException(status_code= status.HTTP_404_NOT_FOUND, detail="invalid post")
-    post_dict = post.dict()
-    post_dict['id'] = id
-    my_posts[index] = post_dict
-    return {"message": "success", "data": post}
+    cursor.execute("""UPDATE posts SET title =%s, content = %s, published=%s WHERE id =%s RETURNING *""",(post.title, post.content, post.published, str(id)))
+    updated_posts = cursor.fetchone()
+    print(updated_posts)
+    conn.commit()
+    if updated_posts ==  None: 
+        raise HTTPException(status_code= status.HTTP_404_NOT_FOUND, detail=f"posts with the id: {id} does not exist")
+   
+    return {"message": "success", "data": updated_posts}
