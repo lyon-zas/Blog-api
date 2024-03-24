0ba5e2b4e1fd7e9601f0e711743f354014210c93
ted <eyimofeokikiola@gmail.com>
2024-03-24 23:59:32 +0100

    using sqlalchemy

--- app/main.py
+++ app/main.py
@@ -83,23 +83,29 @@ def get_post(id: int,  db: Session = Depends(get_db)):
     return {"message": "success", "data": post}
 
 @app.delete("/post/{id}", status_code= status.HTTP_204_NO_CONTENT)
-def delete_posts(id: int):
+def delete_posts(id: int, db: Session = Depends(get_db)):
     print(id)
-    cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING *""", (str(id),))
-    deleted_post = cursor.fetchone()
-    conn.commit()
-    if deleted_post ==  None: 
+    # cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING *""", (str(id),))
+    # deleted_post = cursor.fetchone()
+    # conn.commit()
+    post = db.query(models.Post).filter(models.Post.id ==  id)
+
+    if post.first() ==  None: 
         raise HTTPException(status_code= status.HTTP_404_NOT_FOUND, detail=f"posts with the id: {id} does not exist")
-   
+    post.delete(synchronize_session=False)
+    db.commit()
     return Response(status_code=status.HTTP_204_NO_CONTENT) 
 
 @app.put("/posts/{id}")
-def update_posts(id: int, post:PostModel):
-    cursor.execute("""UPDATE posts SET title =%s, content = %s, published=%s WHERE id =%s RETURNING *""",(post.title, post.content, post.published, str(id)))
-    updated_posts = cursor.fetchone()
-    print(updated_posts)
-    conn.commit()
-    if updated_posts ==  None: 
+def update_posts(id: int, new_post:PostModel,  db: Session = Depends(get_db)):
+    # cursor.execute("""UPDATE posts SET title =%s, content = %s, published=%s WHERE id =%s RETURNING *""",(post.title, post.content, post.published, str(id)))
+    # updated_posts = cursor.fetchone()
+    # print(updated_posts)
+    # conn.commit()
+    post_query =  db.query(models.Post).filter(models.Post.id == id)
+    post = post_query.first()
+    if post ==  None: 
         raise HTTPException(status_code= status.HTTP_404_NOT_FOUND, detail=f"posts with the id: {id} does not exist")
-   
-    return {"message": "success", "data": updated_posts}
+    post_query.update(new_post.dict(), synchronize_session=False)
+    db.commit()
+    return {"message": "success", "data": post_query.first()}
