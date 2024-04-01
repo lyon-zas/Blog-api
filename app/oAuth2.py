b63463e2da71d2f86e0cc1687cdbdf2089208879
ted <eyimofeokikiola@gmail.com>
2024-04-01 22:41:30 +0100

    jwt tokens

--- app/oAuth2.py
+++ app/oAuth2.py
@@ -1,4 +1,5 @@
 from jose import JWTError, jwt
+from datetime import datetime, timedelta
 
 #SECRET_KEY
 #Algorithm
@@ -7,3 +8,11 @@ from jose import JWTError, jwt
 SECRET_KEY = "cb7ee919fcc5a34b9d4508ed3bfb114e705f355b1ce46e98297a198d59c5619b"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30 
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+
+    expire = datetime.now() + timedelta(minutes = ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp":expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI, Response, status, HTTPException, Depends, APIRouter
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 from fastapi.params import Body
 
 
 from sqlalchemy.orm import Session
-from .. import models, schemas, utils
+from .. import models, schemas, utils, oAuth2
 from ..database import get_db
 
 router = APIRouter(prefix= "/auth", tags= ["Auth"])
@@ -19,14 +20,15 @@ def register_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db.refresh(new_user)
     return new_user
 
-@router.post("/login", status_code= status.HTTP_201_CREATED, response_model=schemas.UserResponse)
-def login(user_credentials: schemas.UserLogin, db: Session = Depends(get_db)):
+@router.post("/login", status_code= status.HTTP_201_CREATED, )
+def login(user_credentials:OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
     # hash the password = use.password
-    user =  db.query(models.User).filter(models.User.email == user_credentials.email).first()
+    user =  db.query(models.User).filter(models.User.email == user_credentials.username).first()
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= f"Incorrect email or password")
     if not utils.verify(user_credentials.password, user.password):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= f"Incorrect email or password")
     
     # create a token
-    return user
+    access_token = oAuth2.create_access_token(data= {"user_id": user.id})
+    return {"access_token": access_token, "token_type": "bearer", } 
